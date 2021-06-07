7ca62fb0b568e4d4153b7d83ace63a1d8a04417f
Beny <b.ntambwe97@gmail.com>
2021-06-07 23:29:08 +0200

    test

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,6 @@
     <div class="text-center mx-auto ">
         <h1 class="text-3xl max-w-full d-flex">Bonjour {{ Auth::user()->prenom }} !</h1>
         
-
+        <h2>test</h2>
     </div>
 </x-app-layout>
