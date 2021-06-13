82a597fd5c741449862a39577ba84238f30e0156
Beny <b.ntambwe97@gmail.com>
2021-06-13 20:14:53 +0200

    test pour pull

--- resources/views/template/intro.blade.php
+++ resources/views/template/intro.blade.php
@@ -2,20 +2,20 @@
 <div class="hero-section">
     <div class="hero-content">
         <div class="hero-center">
-            <img src="{{$logo}}" alt="">
+            <img src="{{ $logo }}" alt="">
             <p>
                 @foreach ($carousel as $car)
                     @if ($car->principal == 1)
-                        {{ $car->titre}}
+                        {{ $car->titre }}
                     @endif
                 @endforeach
-        </p>
+            </p>
         </div>
     </div>
     <!-- slider -->
     <div id="hero-slider" class="owl-carousel">
         @foreach ($carousel as $item)
-        <div class="item  hero-item" data-bg="{{$item->img}}"></div>
+            <div class="item  hero-item" data-bg="{{ $item->img }}"></div>
         @endforeach
     </div>
 </div>
